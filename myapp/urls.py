56bc1e22ad7af0728116535e2687601e2bfb10fe
EL BAHTI Ayoub <79053668+ayoubElbahti@users.noreply.github.com>
2024-05-18 19:54:24 +0100

    Update urls.py

--- myapp/urls.py
+++ myapp/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('download_ig_video/', views.download_ig_video, name='download_ig_video'),
     path('download_tik_video/<str:url>/', views.download_tik_video, name='download_tik_video'),
     path('download_yb_video/<path:url>/', views.download_yb_video, name='download_yb_video'),
+    path('translate/<str:input_text>/<str:from_lg>/<str:to_lg>/', views.translate, name='translate'),
 ]
